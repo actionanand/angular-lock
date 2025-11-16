6b3a2eba22dd091016396f3d00b29dc7617eb419
actionanand <meetanandns@gmail.com>
2025-11-16 13:31:39 +0000

    skeleton with dummy pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', loadComponent: () => import('./pages/about/about').then(m => m.About) },
+  { path: '**', redirectTo: '' }
+];
